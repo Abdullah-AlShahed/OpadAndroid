9d187fd9cb0fe9b0b900eebf715c8d413c9ecdb3
Opad Samy <opad.samy@gmail.com>
2023-07-29 14:13:49 +0300

    opad App. V -10.0

---
+++ app/src/main/java/com/example/opad/route_app/RouteAppMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.opad.route_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.opad.R;
+
+public class RouteAppMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_app_main);
+    }
+}
---
+++ app/src/main/java/com/example/opad/route_app/SplashRouteAppActivity.java
@@ -0,0 +1,16 @@
+package com.example.opad.route_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.opad.R;
+
+public class SplashRouteAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_route_app);
+    }
+}
