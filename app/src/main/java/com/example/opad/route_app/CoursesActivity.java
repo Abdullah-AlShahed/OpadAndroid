815ed47619a234b7b877680a7ef14a1b7880a613
Opad Samy <opad.samy@gmail.com>
2023-08-02 11:56:06 +0300

    opad App. V 0.0

--- app/src/main/java/com/example/opad/route_app/CoursesActivity.java
+++ app/src/main/java/com/example/opad/route_app/CoursesActivity.java
@@ -2,15 +2,28 @@ package com.example.opad.route_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.opad.R;
 
 public class CoursesActivity extends AppCompatActivity {
 
+    ImageView showedImage;
+    TextView showedText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
+    showedImage=findViewById(R.id.course_img);
+    showedText=findViewById(R.id.course_txt);
+
+    showedImage.setImageResource(getIntent().getIntExtra("courseImg",-1));
+    showedText.setText(getText(getIntent().getIntExtra("courseBtnText",-1)));
+
     }
 }
--- app/src/main/java/com/example/opad/route_app/RouteAppMainActivity.java
+++ app/src/main/java/com/example/opad/route_app/RouteAppMainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.opad.R;
@@ -27,25 +28,34 @@ public class RouteAppMainActivity extends AppCompatActivity {
         routeCoursesRecyclerView = findViewById(R.id.route_course_rv);
         creatRouteCourses();
         adapter = new RouteAppRecyclerAdapter(coursesList);
-        layoutManager =new LinearLayoutManager(this);
+        layoutManager = new LinearLayoutManager(this);
+        Intent intent = new Intent(RouteAppMainActivity.this, CoursesActivity.class);
         adapter.setOnButtonClickListener(new RouteAppRecyclerAdapter.OnItemClickListener() {
             @Override
             public void onClick(int position, CourseData item) {
-                
+                if (item.courseBtnText==R.string.android_course_text) {
+                    intent.putExtra("courseImg", R.drawable.android);
+                    intent.putExtra("courseBtnText", R.string.android_content);
+                }
+                if (item.courseBtnText==R.string.ios_course_text) {
+                    intent.putExtra("courseImg", R.drawable.ios);
+                    intent.putExtra("courseBtnText", R.string.ios_content);
+                }
+                if (item.courseBtnText==R.string.fullStack_course_text) {
+                    intent.putExtra("courseImg", R.drawable.full_stack);
+                    intent.putExtra("courseBtnText", R.string.fullStack_content);
+                }
+                startActivity(intent);
             }
         });
-
-
-
         routeCoursesRecyclerView.setAdapter(adapter);
         routeCoursesRecyclerView.setLayoutManager(layoutManager);
     }
 
     private void creatRouteCourses() {
         coursesList = new ArrayList<>();
-        coursesList.add(new CourseData(R.drawable.android,R.string.android_course_text));
-        coursesList.add(new CourseData(R.drawable.ios,R.string.ios_course_text));
-        coursesList.add(new CourseData(R.drawable.full_stack,R.string.fullStack_course_text));
+        coursesList.add(new CourseData(R.drawable.android, R.string.android_course_text));
+        coursesList.add(new CourseData(R.drawable.ios, R.string.ios_course_text));
+        coursesList.add(new CourseData(R.drawable.full_stack, R.string.fullStack_course_text));
     }
-
 }
